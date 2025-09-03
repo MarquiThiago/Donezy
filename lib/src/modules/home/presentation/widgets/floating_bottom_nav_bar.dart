5f382ea182a20676ec5b3d68d543fd15f8d8b944
Thiago Marqui <thiago.smarqui21@gmail.com>
2025-09-03 10:15:53 -0300

    fix

--- lib/src/modules/home/presentation/widgets/floating_bottom_nav_bar.dart
+++ lib/src/modules/home/presentation/widgets/floating_bottom_nav_bar.dart
@@ -15,40 +15,63 @@ class FloatingBottomNavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(
-        horizontal: DSSpace.xSmall,
-        vertical: DSSpace.medium,
-      ),
-      decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.surface,
-        borderRadius: BorderRadius.all(DSProperty.radiusXLarge),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black45,
-            blurRadius: 25,
-            offset: const Offset(8, 20),
+    return Row(
+      children: [
+        Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: DSSpace.xxSmall,
+            vertical: DSSpace.small,
           ),
-        ],
-      ),
-      child: GNav(
-        backgroundColor: Colors.transparent,
-        padding: EdgeInsetsGeometry.symmetric(
-          horizontal: DSSpace.large,
-          vertical: DSSpace.medium,
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.surface,
+            borderRadius: BorderRadius.all(DSProperty.radiusXLarge),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black45,
+                blurRadius: 25,
+                offset: const Offset(0, 10),
+              ),
+            ],
+          ),
+          child: GNav(
+            backgroundColor: Colors.transparent,
+            padding: EdgeInsetsGeometry.symmetric(
+              horizontal: DSSpace.large,
+              vertical: DSSpace.medium,
+            ),
+            gap: DSSpace.xxSmall,
+            tabMargin: EdgeInsets.symmetric(horizontal: DSSpace.xSmall),
+            tabBackgroundColor: Theme.of(context).colorScheme.primary,
+            activeColor: Theme.of(context).colorScheme.onPrimary,
+            color: Theme.of(context).colorScheme.onSurfaceVariant,
+            tabs: [
+              GButton(icon: Icons.notes, text: ConstStrings.notes),
+              GButton(icon: Icons.task, text: ConstStrings.tasks),
+              GButton(icon: Icons.person, text: ConstStrings.profile),
+            ],
+            onTabChange: onTabChange,
+          ),
         ),
-        gap: DSSpace.xSmall,
-        tabMargin: EdgeInsets.symmetric(horizontal: DSSpace.medium),
-        tabBackgroundColor: Theme.of(context).colorScheme.primary,
-        activeColor: Theme.of(context).colorScheme.onPrimary,
-        color: Theme.of(context).colorScheme.onSurfaceVariant,
-        tabs: [
-          GButton(icon: Icons.notes, text: ConstStrings.notes),
-          GButton(icon: Icons.task, text: ConstStrings.tasks),
-          GButton(icon: Icons.person, text: ConstStrings.profile),
-        ],
-        onTabChange: onTabChange,
-      ),
+        DSBoxSpace(),
+        Container(
+          padding: EdgeInsets.all(DSSpace.medium),
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.primary,
+            borderRadius: BorderRadius.circular(DSSpace.xLarge),
+            // boxShadow: [
+            //   BoxShadow(
+            //     color: Colors.black45,
+            //     blurRadius: 25,
+            //     offset: const Offset(0, 10),
+            //   ),
+            // ],
+          ),
+          child: Icon(
+            Icons.add,
+            color: Theme.of(context).colorScheme.onPrimary,
+          ),
+        ),
+      ],
     );
   }
 }
