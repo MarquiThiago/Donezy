22d9def3b91311a06a84111e89475df9de64d3ab
Thiago <thiago.smarqui21@gmail.com>
2025-12-18 12:45:17 -0300

    feat(const): Add constant file for strings in the app

---
+++ lib/src/modules/common/presentation/consts/strings.dart
@@ -0,0 +1,8 @@
+class Strings {
+  static const String appName = 'Donezy';
+
+  // Profile Constants Strings
+  static const String profileTitle = 'Profile';
+  static const String changePassword = 'Change Password';
+  static const String settings = 'Settings';
+}
